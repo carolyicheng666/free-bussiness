55cd5e5a0fd14515a00c8a3acc1ea333499de4bf
carolyicheng666 <790496165@qq.com>
2018-02-08 14:30:23 +0800

    chore: 尝试使用npm引入

--- gulpfile.js
+++ gulpfile.js
@@ -20,6 +20,36 @@ gulp.task('sass', function() {
     }))
 });
 
+// Windows下会报 Error: EISDIR: illegal operation on a directory, read 的错误，暂时找不到解决的办法
+// gulp.task('copy', function() {
+//   gulp.src([
+//       'node_modules/bootstrap/dist/**/*',
+//       '!**/npm.js',
+//       '!**/bootstrap-theme.*',
+//       '!**/*.map'
+//     ])
+//     .pipe(gulp.dest('vendor/bootstrap'))
+
+//   gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+//     .pipe(gulp.dest('vendor/jquery'))
+
+//   gulp.src(['node_modules/jquery.easing/*.js'])
+//     .pipe(gulp.dest('vendor/jquery-easing'))
+
+//   gulp.src([
+//       'node_modules/font-awesome/**',
+//       '!node_modules/font-awesome/**/*.map',
+//       '!node_modules/font-awesome/.npmignore',
+//       '!node_modules/font-awesome/*.txt',
+//       '!node_modules/font-awesome/*.md',
+//       '!node_modules/font-awesome/*.json'
+//     ])
+//     .pipe(gulp.dest('vendor/font-awesome'))
+
+//   gulp.src(['node_modules/normalize.css/normalize.css'])
+//     .pipe(gulp.dest('vendor/normalize'))
+// });
+
 gulp.task('browserSync', function() {
   browserSync.init({
     server: {
